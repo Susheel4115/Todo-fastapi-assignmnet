d7a82dd8bc06121e91cda334bd8c4e8f2734b7c5
Susheel Kumar <74233895+Susheel4115@users.noreply.github.com>
2022-11-22 11:09:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 # main.py
 
-from fastapi import FastAPI, Request, Depends, Form, status
-from fastapi.templating import Jinja2Templates
-import models
-from database import engine, sessionlocal
-from sqlalchemy.orm import Session
-from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
+from sqlalchemy.orm import Session
+from database import engine, sessionlocal
+import models
+from fastapi import FastAPI, Request, Depends, Form, status
+# Import Jinja2Templates.
+# Create a templates object that you can re-use later.
+# Declare a Request parameter in the path operation that will return a template.
+# Use the templates you created to render and return a TemplateResponse, passing the request as one of the key-value pairs in the Jinja2 "context".
+from fastapi.templating import Jinja2Templates
+
+
+# FastAPI will use this response_model to: Convert the output data to its type declaration. Validate the data. Add a JSON Schema for the response, in the OpenAPI path operation.
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -14,7 +21,13 @@ templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 
+
+# mounting the static css file to the main.py to display the output
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# creating a new session for every instance of db.
+
 
 def get_db():
     db = sessionlocal()
@@ -23,11 +36,16 @@ def get_db():
     finally:
         db.close()
 
- #we can declare the type of a path parameter in the function, using standard Python type annotations:
+
+# route to get all the todo
+
 @app.get("/")
 async def home(request: Request, db: Session = Depends(get_db)):
     todos = db.query(models.Todo).order_by(models.Todo.id.desc())
     return templates.TemplateResponse("index.html", {"request": request, "todos": todos})
+
+# add todo using post method
+
 
 @app.post("/add")
 async def add(request: Request, task: str = Form(...), db: Session = Depends(get_db)):
@@ -36,10 +54,18 @@ async def add(request: Request, task: str = Form(...), db: Session = Depends(get
     db.commit()
     return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
 
+# When you declare other function parameters that are not part of the path parameters, they are automatically interpreted as "query" parameters.
+
+# edit a specific todo item
+
+
 @app.get("/edit/{todo_id}")
 async def add(request: Request, todo_id: int, db: Session = Depends(get_db)):
     todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
     return templates.TemplateResponse("edit.html", {"request": request, "todo": todo})
+
+# checking the todo is complete or not
+
 
 @app.post("/edit/{todo_id}")
 async def add(request: Request, todo_id: int, task: str = Form(...), completed: bool = Form(False), db: Session = Depends(get_db)):
@@ -49,6 +75,8 @@ async def add(request: Request, todo_id: int, task: str = Form(...), completed: 
     db.commit()
     return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
 
+
+# deleting a particular todo using db.delete
 @app.get("/delete/{todo_id}")
 async def add(request: Request, todo_id: int, db: Session = Depends(get_db)):
     todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
